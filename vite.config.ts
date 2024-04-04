39706f6b101595b4f09359c094fbfc1c27b533ee
Rk <36420273+miniMax18@users.noreply.github.com>
2024-04-04 15:30:55 +0200

    ready_to_deploy_refactor011_vite_ts_docker: deleted non used folder dockerfiles with files, del other *.md, *.html non used, fix the code or comments in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,20 +8,7 @@ export default defineConfig(({ mode }) => {
     base: './',
     plugins: [react()],
     define: {
-      // 'process.env.YOUR_STRING_VARIABLE': JSON.stringify(env.YOUR_STRING_VARIABLE),
-      // 'process.env.YOUR_BOOLEAN_VARIABLE': env.YOUR_BOOLEAN_VARIABLE,
-      // If you want to exposes all env variables, which is not recommended
-      //'process.env.REACT_APP_MOCK': //JSON.stringify(env.REACT_APP_MOCK),
-      //'process.env.REACT_APP_MOCK': env.REACT_APP_MOCK,
       'process.env': env,
-      //'meta': import.meta.env
-      //'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
-    },
- //   server: {
- //     port: 3000,
- //     strictPort: true,
- //     host: true,
- //     origin: "http://0.0.0.0:3000",
- //}
+    }
   }
 })
