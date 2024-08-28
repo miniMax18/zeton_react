fea496e9b49e3b383dcdd217e889924badb33bd1
Martyna <110538495+martinafm@users.noreply.github.com>
2024-08-28 21:35:35 +0200

    routing refactor

--- src/const/routing.const.ts
+++ src/const/routing.const.ts
@@ -1,10 +1,17 @@
-export const ROUTE_NAME = {
+export const BASE_ROUTES_NAME = {
   home: '/',
   login: '/login',
   student: '/:id',
-  tasks: '/:id/tasks',
-  awards: '/:id/awards',
-  consequences: '/:id/consequences',
-  info: '/:id/info',
-  settings: '/:id/settings',
+};
+export const SUB_ROUTES_NAME = {
+  awards: '/awards',
+  consequences: '/consequences',
+  info: '/info',
+  settings: '/settings',
+  tasks: '/tasks',
+};
+
+export const AWARDS_SUB_ROUTES = {
+  add: '/add',
+  grant: '/grant',
 };
