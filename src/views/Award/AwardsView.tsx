93a77a8b87646ad7a6a774e50bde35542003dd75
Martyna <110538495+martinafm@users.noreply.github.com>
2024-08-28 21:43:48 +0200

    prepared award routing

--- src/views/Award/AwardsView.tsx
+++ src/views/Award/AwardsView.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import HomeTemplate from '../../components/templates/HomeTemplate.tsx';
 import StudentHeader from '../../components/structures/StudentHeader/StudentHeader.tsx';
 import Loading from '../../components/atoms/Loading/Loading.tsx';
 import Navbar from '../../components/structures/Navbar/Navbar.tsx';
 import { useStudentById } from '../../api/useStudentById.ts';
 import Button from '../../components/atoms/Buttons/Button.ts';
+import { AWARDS_SUB_ROUTES } from '../../const/routing.const.ts';
 
 const AwardsView = () => {
   const { id } = useParams();
   const { student, isStudentLoading, isStudentError } = useStudentById(id);
+
+  const navigate = useNavigate();
 
   return (
     <HomeTemplate>
@@ -21,8 +24,12 @@ const AwardsView = () => {
           studentId={id}
         />
       )}
-      <Button>Dodaj nagrodę</Button>
-      <Button>Przyznaj nagrodę</Button>
+      <Button onClick={() => navigate(`.${AWARDS_SUB_ROUTES.add}`)}>
+        Dodaj nagrodę
+      </Button>
+      <Button onClick={() => navigate(`.${AWARDS_SUB_ROUTES.grant}`)}>
+        Przyznaj nagrodę
+      </Button>
       <Navbar />
     </HomeTemplate>
   );
